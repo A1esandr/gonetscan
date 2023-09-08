29b40d47e2205847c5929d9c1bc0b6d184574de2
A1esandr <a1exswan001@gmail.com>
2023-09-08 10:46:26 +0500

    added flag part

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/A1esandr/gonetscan"
 )
 
+var address string
+
+func init() {
+	flag.StringVar(&address, "address", "127.0.0.1", "address to scan")
+}
+
 func main() {
+	flag.Parse()
 	ports := make([]int, 0, 10000)
 	for i := 1; i < 1000; i++ {
 		ports = append(ports, i)
