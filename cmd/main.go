e4aa900bae7f6de3ffd4995a3cea9144bcef0c65
A1esandr <a1exswan001@gmail.com>
2023-09-12 10:49:11 +0500

    result write

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"math"
+	"os"
+	"time"
 
 	"github.com/A1esandr/gonetscan"
 )
@@ -17,16 +20,32 @@ func init() {
 func main() {
 	flag.Parse()
 	ports := make([]int, 0, 10000)
-	fmt.Println(int(math.Pow(2, 10)))
-	for i := 1; i < int(math.Pow(2, 10)); i++ {
+	fmt.Println(int(math.Pow(2, 16)))
+	for i := 1; i < int(math.Pow(2, 16)); i++ {
 		ports = append(ports, i)
 	}
 	result := gonetscan.NewScanner().Scan(address, ports)
+	data := make([]string, 0, len(result.Open)+2)
 	if len(result.Open) > 0 {
 		fmt.Println("Open")
+		data = append(data, "Open")
 	}
 	for _, v := range result.Open {
 		fmt.Printf("%s:%d\n", address, v)
+		data = append(data, fmt.Sprintf("%s:%d\n", address, v))
 	}
+	data = append(data, "Finish")
+	write(data)
 	fmt.Println("Finish")
+}
+
+func write(data []string) {
+	file, err := os.Create("./result" + time.Now().String() + "./.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	for _, line := range data {
+		file.WriteString(line + "\n")
+	}
 }
